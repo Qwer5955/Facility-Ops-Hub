e547a9be751fe31a25f00f49ba7ba4c022f6077f
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2025-12-02 12:51:46 +0530

    Setted up the backend and created user and other entities

--- src/main/java/com/facilityops/facility_ops_hub/models/User.java
+++ src/main/java/com/facilityops/facility_ops_hub/models/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "users")
 public class User {
-
+    //this is the user entity i have created
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
